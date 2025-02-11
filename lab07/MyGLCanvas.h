159baa55a731480a9a94db8e3742588cebdfa3b4
ileisus <96794006+ileisus@users.noreply.github.com>
2025-02-11 02:21:02 -0500

    merged lab7 branch

---
+++ lab07/MyGLCanvas.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#ifndef MYGLCANVAS_H
+#define MYGLCANVAS_H
+
+#if defined(__APPLE__)
+#  include <OpenGL/gl3.h> // defines OpenGL 3.0+ functions
+#else
+#  if defined(WIN32)
+#    define GLEW_STATIC 1
+#  endif
+#  include <GL/glew.h>
+#endif
+#include <FL/glut.h>
+#include <FL/glu.h>
+#include <glm/glm.hpp>
+#include <time.h>
+#include <iostream>
+
+#include "SceneObject.h"
+#include "ShaderManager.h"
+#include "ply.h"
+#include "gfxDefs.h"
+
+
+class MyGLCanvas : public Fl_Gl_Window {
+public:
+    // added for light color control 
+    glm::vec3 lightColor;  
+
+	// Length of our spline (i.e how many points do we randomly generate)
+
+
+	glm::vec3 eyePosition;
+	glm::vec3 rotVec;
+	glm::vec3 lookatPoint;
+	glm::vec3 lightPos;
+
+	int viewAngle;
+	int animateLight;
+	float clipNear;
+	float clipFar;
+	float scaleFactor;
+
+	MyGLCanvas(int x, int y, int w, int h, const char *l = 0);
+	~MyGLCanvas();
+
+	void reloadShaders();
+
+private:
+	void draw();
+	void drawScene();
+
+	void initShaders();
+
+	int handle(int);
+	void resize(int x, int y, int w, int h);
+	void updateCamera(int width, int height);
+
+	SceneObject* myObject;
+	ShaderManager* myShaderManager;
+	ply* myPLY;
+	bool firstTime;
+};
+
+#endif // !MYGLCANVAS_H
